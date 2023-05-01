509d8a7aa8efc573c7f94ebd7bd6edf61952403a
Oliver <65511074+oliverpatrick@users.noreply.github.com>
2023-05-02 00:58:12 +0100

    Update HomePage.tsx
    Lazy load hero’s with suspense (mobile change - test on desktop)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Hero from "../components/Hero/Hero";
 import Layout from "../components/Layout";
+
+const Hero = React.lazy(() => import("../components/Hero/Hero"))
 
 function HomePage() {
   return (
-    <>
+    <Suspense fallback={<h2>Loading...</h2>}>
       <Link to="/launches">
         <Hero image="./images/menu-1.jpg" text="Launches" imageAlt="launch" />
       </Link>
       <Link to="/rockets">
         <Hero image="./images/menu-2.jpg" text="Rockets" imageAlt="rockets" />
       </Link>
-    </>
+    </Suspense>
   );
 }
 
